b6eed8d3055ce950fa0cecfbcfc3444d51b0fd11
Vladimir <warmn@bk.ru>
2020-12-10 20:27:57 +0500

    lucky number

--- Stepik.cpp
+++ Stepik.cpp
@@ -3,18 +3,28 @@
 int main() {
 	using namespace std;
 
-	int min_age, new_age;
 
-	cin >> new_age;
-	min_age = new_age;
-	while (new_age != 0)
+	int first_part, second_part;
+	cin >> first_part >> second_part;
+	int sum1 = 0, sum2 = 0;
+
+	while (first_part != 0)
 	{
-		if (min_age > new_age)
-		{
-			min_age = new_age;
-		}
-		cin >> new_age;
+		sum1 = sum1 + first_part % 10;
+		first_part /= 10;
 	}
-	cout << min_age;
+	while (second_part != 0)
+	{
+		sum2 = sum2 + second_part % 10;
+		second_part /= 10;
+	}
+	if (sum1 == sum2)
+	{
+		cout << "eat" << endl;
+	}
+	else
+	{
+		cout << "unlacky(" << endl;
+	}
 	return 0;
 }
